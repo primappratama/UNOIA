0224d07551a4200af97d14180a2d69004efeea02
eienorion <muchamadalfaidzin@gmail.com>
2025-06-29 04:54:28 +0700

    Initial commit

---
+++ app/src/main/java/muchamadalfaidzin/informatika/tilas/LandPageActivity.kt
@@ -0,0 +1,29 @@
+package muchamadalfaidzin.informatika.tilas
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LandPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tvLogin = findViewById<TextView>(R.id.tvLogin)
+
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
